1c049453f02f26b398f64854a363f772e48167d4
Robert Rouhani <robert.rouhani@gmail.com>
2014-09-06 17:35:03 -0400

    Changed URL... pls work azure

--- mozaical_app/mozaical_app/urls.py
+++ mozaical_app/mozaical_app/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mozaical_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^moz/', include(moz.urls)),
+    url(r'^', include(moz.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
